0fd625c063bd81f2c632a8cb4914f53fd1eebeaa
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-12-26 09:06:39 +0900

    CreateBooks関数の実装とテスト用のデータを準備した

--- handler/handler.go
+++ handler/handler.go
@@ -10,3 +10,17 @@ import (
 func GetBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, models.Books)
 }
+
+func CreateBooks(c *gin.Context) {
+
+	var newBook models.Book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books := append(models.Books, newBook)
+
+	c.IndentedJSON(http.StatusCreated, books)
+
+}
--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/books", handler.GetBooks)
+	r.POST("/books", handler.CreateBooks)
 
 	r.Run()
 
